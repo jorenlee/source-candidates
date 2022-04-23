fdc6c971868e37148f985e8c153e5cec73de8444
jorenlee24 <jo@bluthrive.com>
2022-04-23 04:31:31 -0700

    initial

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center border border-black">
+    Test
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
